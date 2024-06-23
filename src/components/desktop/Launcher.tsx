8cd6520802e7376c9e4088e0581e265518691cb2
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-23 14:14:17 -0400

    Installing framer motion and using some motion

--- src/components/desktop/Launcher.tsx
+++ src/components/desktop/Launcher.tsx
@@ -1,7 +1,12 @@
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import chatIcon from "pixelarticons/svg/chat.svg";
 import musicIcon from "pixelarticons/svg/music.svg";
 
 const Launcher = () => {
+  // Framer Motion
+  let mouseX = useMotionValue(0);
+  let width = useTransform(mouseX, [200, 250, 400], [20, 40, 20]);
+
   // The elements that are being shown at the button
   const elements: Option[] = [
     {
@@ -12,17 +17,42 @@ const Launcher = () => {
       name: "music",
       component: <img className="" src={musicIcon} />,
     },
+    {
+      name: "music",
+      component: <img className="" src={musicIcon} />,
+    },
+    {
+      name: "music",
+      component: <img className="" src={musicIcon} />,
+    },
+    {
+      name: "music",
+      component: <img className="" src={musicIcon} />,
+    },
   ];
 
+  // Functions
+  // const updateMousePosition()
+
   return (
-    <div className="fixed bottom-10 mt-auto">
-      <ul className="backdrop-blur-sm bg-red bg-opacity-60 rounded-md mx-auto flex gap-4 h-16 items-end px-4 pb-3">
+    <div
+      onMouseMove={(e) => {
+        mouseX.set(e.pageX);
+        console.log(e.pageX);
+      }}
+      className="fixed bottom-10 mt-auto"
+    >
+      <motion.ul className="backdrop-blur-sm bg-red bg-opacity-60 rounded-md mx-auto flex gap-4 h-16 items-end px-4 pb-3">
         {elements.map((elem, index) => (
-          <li key={index} className="aspect-square w-10 rounded-full">
+          <motion.li
+            style={{ width }}
+            key={index}
+            className="aspect-square w-10 rounded-full"
+          >
             {elem.component}
-          </li>
+          </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </div>
   );
 };
