2d1e18b473837a2206cd490052188855ca893ca8
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-23 14:34:09 -0400

    Updated the lancher animation

--- src/components/desktop/Launcher.tsx
+++ src/components/desktop/Launcher.tsx
@@ -1,11 +1,17 @@
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import {
+  MotionValue,
+  motion,
+  useMotionValue,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import chatIcon from "pixelarticons/svg/chat.svg";
 import musicIcon from "pixelarticons/svg/music.svg";
+import { useRef } from "react";
 
 const Launcher = () => {
   // Framer Motion
-  let mouseX = useMotionValue(0);
-  let width = useTransform(mouseX, [200, 250, 400], [20, 40, 20]);
+  let mouseX = useMotionValue(Infinity);
 
   // The elements that are being shown at the button
   const elements: Option[] = [
@@ -31,29 +37,55 @@ const Launcher = () => {
     },
   ];
 
-  // Functions
-  // const updateMousePosition()
-
   return (
     <div
-      onMouseMove={(e) => {
-        mouseX.set(e.pageX);
-        console.log(e.pageX);
-      }}
+      onMouseMove={(e) => mouseX.set(e.pageX)}
+      onMouseLeave={() => mouseX.set(Infinity)}
       className="fixed bottom-10 mt-auto"
     >
       <motion.ul className="backdrop-blur-sm bg-red bg-opacity-60 rounded-md mx-auto flex gap-4 h-16 items-end px-4 pb-3">
         {elements.map((elem, index) => (
-          <motion.li
-            style={{ width }}
-            key={index}
-            className="aspect-square w-10 rounded-full"
-          >
-            {elem.component}
-          </motion.li>
+          <AppIcon key={index} component={elem.component!} mouseX={mouseX} />
         ))}
       </motion.ul>
     </div>
+  );
+};
+
+interface AppIconProps {
+  component: JSX.Element;
+  mouseX: MotionValue;
+}
+
+const AppIcon = ({ component, mouseX }: AppIconProps) => {
+  // REFS
+  const ref = useRef<HTMLLIElement>(null);
+
+  // FRAMER MOTION
+  // The distance between the mouse and the actual launcher icon
+  const distance = useTransform(mouseX, (val) => {
+    const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
+    const pos = val - bounds.x - bounds.width * 0.5;
+
+    return pos;
+  });
+
+  // The width of the icon when the mouse hovers over
+  const widthSync = useTransform(distance, [-100, 0, 100], [40, 80, 40]);
+  const width = useSpring(widthSync, {
+    damping: 20,
+    mass: 0.5,
+    stiffness: 200,
+  });
+
+  return (
+    <motion.li
+      ref={ref}
+      style={{ width }}
+      className="aspect-square w-10 rounded-full"
+    >
+      {component}
+    </motion.li>
   );
 };
 
