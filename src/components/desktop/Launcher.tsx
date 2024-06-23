1b394124e61de81e7c1439cd2f962749bc4b7df3
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-23 01:07:06 -0400

    Adding the launcher for other apps and stuff

--- src/components/desktop/Launcher.tsx
+++ src/components/desktop/Launcher.tsx
@@ -6,22 +6,20 @@ const Launcher = () => {
   const elements: Option[] = [
     {
       name: "chat",
-      component: (
-        <img className="h-full object-contain cursor-pointer" src={chatIcon} />
-      ),
+      component: <img className="h-full object-contain" src={chatIcon} />,
     },
     {
       name: "music",
-      component: (
-        <img className="h-full object-contain cursor-pointer" src={musicIcon} />
-      ),
+      component: <img className="h-full object-contain" src={musicIcon} />,
     },
   ];
 
   return (
     <div className="fixed bottom-10 mt-auto mx-auto px-5 py-3 w-fit max-w-50 overflow-x-auto h-12 text-center backdrop-blur-sm bg-red bg-opacity-60 rounded-md">
       <div className="flex flex-row gap-4 h-full">
-        {elements.map((elem) => elem.component)}
+        {elements.map((elem) => (
+          <div className="h-full cursor-pointer">{elem.component}</div>
+        ))}
       </div>
     </div>
   );
