bc062fa0f8c18ed118a60338c74e0d7a5b127ac1
Marcos Cevallos <mjcevallos007@gmail.com>
2024-07-01 11:02:45 -0400

    Updated some text

--- src/pages/death/BlueScreen.tsx
+++ src/pages/death/BlueScreen.tsx
@@ -5,9 +5,9 @@ const BlueScreenDeath = () => {
     <div className="w-full h-screen bg-[#3366ff] flex flex-col justify-center gap-14 p-4">
       <h2 className="text-9xl text-white">:(</h2>
 
-      <h3 className="text-white text-xl">My website ran into a problem displaying this page. You can go back to the main page by clicking on the link below. If you believe that this should be a valid address, you can contact meand let me know</h3>
+      <h3 className="text-white text-xl">My website ran into a problem displaying this page. You can go back to the main page by clicking on the link below. If you believe that this should be a valid web page, you can let me know!</h3>
 
-      <h5 className="text-white text-lg"> Go back to the home page by <Link to="/Home" className="underline">Clicking Here</Link>.</h5>
+      <h5 className="text-white text-lg"> Go back to the home page by <Link to="/Home" className="underline">clicking here</Link>.</h5>
     </div>
   )
 }
