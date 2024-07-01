72c4575da4f29445ebaca9841d8a49fc170f9515
Marcos Cevallos <mjcevallos007@gmail.com>
2024-06-30 21:55:59 -0400

    added a blue screen of death

--- src/pages/death/BlueScreen.tsx
+++ src/pages/death/BlueScreen.tsx
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom"
+
 const BlueScreenDeath = () => {
   return (
-    <div>BlueScreenDeath</div>
+    <div className="w-full h-screen bg-[#3366ff] flex flex-col justify-center gap-9 p-4">
+      <h2 className="text-9xl text-white">:(</h2>
+
+      <h3 className="text-white text-xl">My website ran into a problem displaying this page. You can go back to the main page by clicking on the link below. If you believe that this should be a valid address, you can contact meand let me know</h3>
+
+      <h5 className="text-white text-lg"> Go back to the home page by <Link to="/Home" className="underline">Clicking Here</Link>.</h5>
+    </div>
   )
 }
 
