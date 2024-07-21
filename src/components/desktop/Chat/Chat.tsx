da028d34d5c388d1d485a633d480ad0ffe429075
Marcos Cevallos <mjcevallos007@gmail.com>
2024-07-21 00:23:50 -0400

    Upadting the chat feature

--- src/components/desktop/Chat/Chat.tsx
+++ src/components/desktop/Chat/Chat.tsx
@@ -1,5 +1,12 @@
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <div className="rounded-lg bg-green p-6">
+      {/* Header */}
+      <div className="text-3xl text-white">Let's Connect!</div>
+
+      {/* Content */}
+    </div>
+  );
 };
 
 export default Chat;
