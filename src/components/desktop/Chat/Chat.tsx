eb1735a32ee998fb2bab7913665852b0093c57d9
marcosFIF <marcosc@fillitforward.com>
2024-07-21 16:47:07 -0400

    Updated the Messages Chat Conversation

--- src/components/desktop/Chat/Chat.tsx
+++ src/components/desktop/Chat/Chat.tsx
@@ -1,10 +1,23 @@
+import Messages from "./Messages";
+
 const Chat = () => {
+  // STATES
+
+  // FUNCTION
+
   return (
     <div className="rounded-lg bg-green p-6">
       {/* Header */}
-      <div className="text-3xl text-white">Let's Connect!</div>
+      <div className="text-3xl text-white">
+        {/* Profile Icon */}
 
-      {/* Content */}
+        {/* Useless Icons on the right */}
+      </div>
+
+      {/* Message Conversation */}
+      <div>
+        <Messages />
+      </div>
     </div>
   );
 };
