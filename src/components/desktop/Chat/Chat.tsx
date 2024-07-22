27d25b7d2dd18e5c243d9f23687f1016d1ccb306
marcosFIF <marcosc@fillitforward.com>
2024-07-21 21:05:22 -0400

    Updated some more

--- src/components/desktop/Chat/Chat.tsx
+++ src/components/desktop/Chat/Chat.tsx
@@ -17,23 +17,27 @@ const Chat = () => {
       </div>
 
       {/* Message Conversation */}
-      <div className="flex flex-col justify-start overflow-y-scroll whitespace-pre-line w-full">
+
+      <div className="mt-2 flex flex-1 flex-col justify-start overflow-y-scroll whitespace-pre-line w-full gap-3 max-h-full">
         {/* <Messages /> */}
 
-        <div className="bg-blue flex mr-auto w-fit items-center justify-start">
+        <div className="h-fit bg-blue flex mr-auto w-fit max-w-[80%] items-center justify-start overflow-x-auto">
           <img src={humanNormalIcon} className="h-6 w-6 object-contain" />
 
-          <p className="text-xl text-left">this</p>
+          <p className="text-xl text-left">
+            this this is something else that can be said. Yeaaaaa
+          </p>
         </div>
 
         <div className="bg-green flex ml-auto w-fit max-w-[80%] items-center justify-end overflow-x-auto">
-          <img src={humanHandupIcon} className="h-6 w-6 object-contain" />
-
           <p className="text-xl text-left">
             this is my message bask to you dawg. I am going to make it so so
             long for you forever
           </p>
+          <img src={humanHandupIcon} className="h-6 w-6 object-contain" />
         </div>
+
+        {/* Ending */}
       </div>
     </div>
   );
