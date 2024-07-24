2b74a01319ffb830f0d38f8dafb6aabd041bd293
marcosFIF <marcosc@fillitforward.com>
2024-07-23 22:43:52 -0400

    Updating

--- src/components/desktop/MusicPlayer/MusicPlayer.tsx
+++ src/components/desktop/MusicPlayer/MusicPlayer.tsx
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const MusicPlayer = () => {
+  // FUNCTIONS
+  const example = async () => {
+    axios.get("https://accounts.spotify.com/api/token", {
+      headers: {
+        // Authorization: `Basic ${new Buffer.from(}`// TODO: Continue from here
+        // https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+      },
+    });
+  };
+
+  useEffect(() => {}, []);
+
   return <div>MusicPlayer</div>;
 };
 
