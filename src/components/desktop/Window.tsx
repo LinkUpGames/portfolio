87aedd84e589d5dba901bd8835039270668638e5
Marcos Cevallos <mjcevallos007@gmail.com>
2024-07-30 20:15:48 -0400

    Removed a couple of site changes

--- src/components/desktop/Window.tsx
+++ src/components/desktop/Window.tsx
@@ -1,5 +1,4 @@
-import { clampNumber } from "@/helpers/functions";
-import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 interface Props {
   title: string;
@@ -18,13 +17,13 @@ const WindowBorder = ({
   windowClassName = "",
 }: Props) => {
   // CONSTANTS
-  const MAXPOS: Position[] = [
-    { x: 0, y: 20 },
-    {
-      x: window.innerWidth - window.innerWidth * 0.25,
-      y: window.innerHeight - window.innerHeight * 0.2,
-    },
-  ];
+  // const MAXPOS: Position[] = [
+  //   { x: 0, y: 20 },
+  //   {
+  //     x: window.innerWidth - window.innerWidth * 0.25,
+  //     y: window.innerHeight - window.innerHeight * 0.2,
+  //   },
+  // ];
 
   // REFS
   const ref = useRef<HTMLDivElement | null>(null);
@@ -40,41 +39,41 @@ const WindowBorder = ({
   /**
    * Control the position of the div based on the mouse
    */
-  const onMouseDown = useCallback(
-    (event: any) => {
-      const element = ref.current;
-
-      const onMouseMove = (event: MouseEvent) => {
-        pos.x += event.movementX;
-        pos.y += event.movementY;
-
-        pos.x = clampNumber(pos.x, MAXPOS[0].x, MAXPOS[1].x);
-        pos.y = clampNumber(pos.y, MAXPOS[0].y, MAXPOS[1].y);
-
-        if (element && window.innerWidth > 768) {
-          element.style.transform = `translate(${pos.x}px, ${pos.y}px)`;
-          element.style.zIndex = "9999";
-        }
-
-        setPos(pos);
-      };
-
-      const onMouseUp = () => {
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-
-        if (element) {
-          element.style.zIndex = "0";
-        }
-      };
-
-      event.preventDefault();
-
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    },
-    [pos, setPos, ref],
-  );
+  // const onMouseDown = useCallback(
+  //   (event: any) => {
+  //     const element = ref.current;
+  //
+  //     const onMouseMove = (event: MouseEvent) => {
+  //       pos.x += event.movementX;
+  //       pos.y += event.movementY;
+  //
+  //       pos.x = clampNumber(pos.x, MAXPOS[0].x, MAXPOS[1].x);
+  //       pos.y = clampNumber(pos.y, MAXPOS[0].y, MAXPOS[1].y);
+  //
+  //       if (element && window.innerWidth > 768) {
+  //         element.style.transform = `translate(${pos.x}px, ${pos.y}px)`;
+  //         element.style.zIndex = "9999";
+  //       }
+  //
+  //       setPos(pos);
+  //     };
+  //
+  //     const onMouseUp = () => {
+  //       document.removeEventListener("mousemove", onMouseMove);
+  //       document.removeEventListener("mouseup", onMouseUp);
+  //
+  //       if (element) {
+  //         element.style.zIndex = "0";
+  //       }
+  //     };
+  //
+  //     event.preventDefault();
+  //
+  //     document.addEventListener("mousemove", onMouseMove);
+  //     document.addEventListener("mouseup", onMouseUp);
+  //   },
+  //   [pos, setPos, ref],
+  // );
 
   /**
    * Check for a window resizej
