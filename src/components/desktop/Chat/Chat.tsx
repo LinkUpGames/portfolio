31c475a9dac82efe0595f5337a49d7e5b73b9783
Marcos Cevallos <mjcevallos007@gmail.com>
2024-07-31 18:59:30 -0400

    Chat updates
    Updated the chat before we start changing some stuff around

--- src/components/desktop/Chat/Chat.tsx
+++ src/components/desktop/Chat/Chat.tsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Messages from "./Messages";
 import { ChatContext } from "./ChatContext";
 import InputBox from "./InputBox";
+import { IoCloseCircleSharp } from "react-icons/io5";
+import { AppContext } from "@/AppContext";
 
 const Chat = () => {
+  // CONTEXT
+  const { changePopup } = useContext(AppContext);
+
   // STATES
   const [messages, setMessages] = useState<Message[]>([]); // The messages are the going to be displayed
 
@@ -21,6 +26,13 @@ const Chat = () => {
     } else {
       setMessages((prev) => [...prev, message]);
     }
+  };
+
+  /**
+   * Exit the chat app
+   */
+  const handleClick = () => {
+    changePopup(null);
   };
 
   // EFFECTS
@@ -45,7 +57,7 @@ const Chat = () => {
         icon: "https://i.pinimg.com/originals/f4/20/0a/f4200a828929aaa0261aed306bf1c0e5.jpg",
         text: "You can send me a message through here and I'll recieve it!",
       },
-      Math.random() * 6000,
+      Math.random() * 8000,
     );
   }, []);
 
@@ -58,11 +70,16 @@ const Chat = () => {
     >
       <div className="flex flex-col rounded-lg bg-gradient-to-r from-grey to-grey border-2 border-dark h-full w-full">
         {/* Header */}
-        <div className="flex text-3xl bg-white text-black w-full justify-center items-center">
+        <div className="flex text-3xl bg-white text-black w-full justify-center items-center relative">
           {/* Profile Icon */}
 
           {/* Useless Icons on the right */}
-          <div className="uppercase text-2xl p-6 text-center">Messages</div>
+          <h2 className="uppercase text-2xl p-6 text-center">Messages</h2>
+
+          {/* Exit Icon */}
+          <button onClick={handleClick} className="absolute right-4 top-6">
+            <IoCloseCircleSharp size={30} />
+          </button>
         </div>
 
         {/* Message Conversation */}
