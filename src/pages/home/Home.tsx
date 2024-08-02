7ca4493d0fa1f3a3a60554c2c9e69da2dd05fb14
Marcos Cevallos <mjcevallos007@gmail.com>
2024-08-02 00:35:56 -0400

    Updated this part as well

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -38,65 +38,6 @@ const Home = () => {
             </p>
           </div>
 
-          {/* Gaming and Creativity */}
-          <div className="mt-2">
-            <h5 className="text-xl md:text-2xl text-dark font-bold">
-              Fueling Creativity Through Gaming
-            </h5>
-
-            <p className="text-lg md:text-xl">
-              When I'm bored you will probably find me playing&nbsp;
-              <Link
-                to="https://www.ea.com/games/apex-legends?setLocale=en-us?setLocale=en-us"
-                target="_blank"
-                className="underline"
-              >
-                Apex Legends
-              </Link>
-              ,&nbsp; grinding out ranked or bringing my ideas to life with my
-              most <span className="underline">used game engines</span>. Over
-              the years, I have crafted my ideas to life, some of which
-              include&nbsp;
-              <Link
-                to="https://gamejolt.com/games/ultimatereality/406940"
-                target="_blank"
-                className="underline"
-              >
-                Ultimate Reality
-              </Link>
-              ; &nbsp; which turned out to be a hit! I enjoy blending my
-              creativity with code to create immersive experienes that gamers
-              love.
-            </p>
-          </div>
-
-          {/* Tech and Web Development */}
-          <div className="mt-2">
-            <h5 className="text-xl md:text-2xl text-dark font-bold">
-              Tech and Web Development
-            </h5>
-
-            <p className="text-lg md:text-xl">
-              When I'm not gaming, I'm tinkering with tech. I don't want to
-              sound like a broken record, but I swear by Neovim and the
-              increased productivity it offers. <br />
-              My daily driver is Fedora. I enjoy using Fedora because of the
-              stability it offers while also including the latest packages. It's
-              no Arch distro, but it's a really good distro!
-              <br />I am curently working as a full stack software engineer
-              at&nbsp;
-              <Link
-                to="https://fillitforward.com/about/"
-                target="_blank"
-                className="underline text-green"
-              >
-                Fill it Forward
-              </Link>
-              &nbsp;. I enjoy combining sleek design with powerful APIs that
-              work flawlessly.
-            </p>
-          </div>
-
           {/* Pursuit For Challenge */}
           <div className="mt-2">
             <h5 className="text-xl md:text-2xl text-dark font-bold">
@@ -122,8 +63,7 @@ const Home = () => {
             <p className="text-lg md:text-xl">
               Outside of office, I enjoy doing martial arts and skateboarding.
               Martial arts keeps me in shape and disicplined, while skateboading
-              is my outlet for pushing my limits and learning new tricks. Both
-              keep me balanced and ready to take on whatever comes my way.
+              is my outlet for pushing my limits and learning new tricks.
             </p>
           </div>
 
@@ -137,7 +77,7 @@ const Home = () => {
               That's some info about myself and what I'm passionated about! If
               you're into tech, gaming or just want to chat about cool projects,
               feel free to reach out! I'm always up for a good conversation! You
-              can use the little "chat app in the footer!"
+              can use the little chat app at the bottom!
             </p>
           </div>
         </div>
